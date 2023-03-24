99b2afacee4d570da884ecb17127ab727fe666a4
Simonebaca <simobaco99@gmail.com>
2023-03-24 16:49:18 +0100

    milestone 1-2

--- ex-social-posts-starter/main.js
+++ ex-social-posts-starter/main.js
@@ -1,3 +1,18 @@
+/*Descrizione
+Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+Milestone 1
+ Creiamo il nostro array di oggetti che rappresentano ciascun post.
+Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+id del post, numero progressivo da 1 a n
+nome autore,
+foto autore,
+data in formato americano (mm-gg-yyyy),
+testo del post,
+immagine (non tutti i post devono avere una immagine),
+numero di likes.
+Non è necessario creare date casuali Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
+Milestone 2 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.*/
+
 const posts = [
     {
         "id": 1,
@@ -55,3 +70,44 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+let content = "";
+
+posts.forEach((elem) => {
+
+    content +=`
+    <div class="post">
+    <div class="post__header">
+    <div class="post-meta">                    
+    <div class="post-meta__icon">
+    <img class="profile-pic" src="${elem.author.image}" alt="Phil Mangione">                    
+    </div>
+    <div class="post-meta__data">
+    <div class="post-meta__author">${elem.author.name}</div>
+    <div class="post-meta__time">${elem.created}</div>
+    </div>                    
+    </div>
+    </div>
+    <div class="post__text">${elem.content}</div>
+    <div class="post__image">
+    <img src="${elem.media}" alt="">
+    </div>
+    <div class="post__footer">
+    <div class="likes js-likes">
+    <div class="likes__cta">
+    <a class="like-button  js-like-button" href="#" data-postid="1">
+    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+    <span class="like-button__label">Mi Piace</span>
+    </a>
+    </div>
+    <div class="likes__counter">
+    Piace a <b id="like-counter-1" class="js-likes-counter">${elem.likes}</b> persone
+    </div>
+    </div> 
+    </div>  
+    </div>        
+    `
+})
+
+const card = document.getElementById('container');
+card.innerHTML = content;
