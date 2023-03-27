381a54fe8dbff2bfa455b28961468851777c7098
Simonebaca <simobaco99@gmail.com>
2023-03-27 18:19:21 +0200

    fix-js indentation

--- ex-social-posts-starter/main.js
+++ ex-social-posts-starter/main.js
@@ -74,42 +74,36 @@ const posts = [
 let content = "";
 
 posts.forEach((elem) => {
-
-    content += `
-
-            <div class="post">
-             <div class="post__header">
-              <div class="post-meta">                    
-               <div class="post-meta__icon">
-                <img class="profile-pic" src="${elem.author.image}" alt="Phil Mangione">                    
-            </div>
-             <div class="post-meta__data">
-              <div class="post-meta__author">${elem.author.name}</div>
-               <div class="post-meta__time">${elem.created}</div>
-            </div>                    
-             </div>
-              </div>
-            <div class="post__text">${elem.content}</div>
-            <div class="post__image">
-                <img src="${elem.media}" alt="">
-            </div>
-             <div class="post__footer">
-              <div class="likes js-likes">
-               <div class="likes__cta">
-                 <a class="like-button  js-like-button" href="#" data-postid="1">
-                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                         <span class="like-button__label">Mi Piace</span>
-                 </a>
-            </div>
-             <div class="likes__counter">
-                 Piace a <b id="like-counter-1" class="js-likes-counter">${elem.likes}</b> persone
-             </div>
-              </div> 
-               </div>  
-                </div> 
-                       
-        `
+    content +=`
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${elem.author.image}" alt="Phil Mangione">     
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${elem.author.name}</div>
+                    <div class="post-meta__time">${elem.created}</div>
+                </div>                    
+            </div>               
+        </div>
+        <div class="post__text">${elem.content}</div>
+        <div class="post__image">
+            <img src="${elem.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a  class="like-button  js-like-button" href="#" data-postid="${elem.id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${elem.id}" class="js-likes-counter">${elem.likes}</b> persone
+                </div>
+            </div> 
+        </div>  
+    </div>        
+    `
 })
-
-const card = document.getElementById('container');
-card.innerHTML = content;
